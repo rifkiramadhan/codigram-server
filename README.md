42fb9caf4082b46d4968a92fad02db20b45c197b
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2022-02-08 16:19:03 +0700

    Mengirim project server-side

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tugas Social Media
+
+## Server Social Media
+
+### Rancangan Data
+
+- [ERD](https://drive.google.com/file/d/1xtUpR-D0lAiq2loQG54Eq2ZOTGmH2aUG/view?usp=sharing)
+- [API Documentation](https://documenter.getpostman.com/view/10749611/UVeJJjuj)
+
+### Cara Installasi
+
+- npm init -y
+- npm install express cors mysql2
+- npm install nodemon
+- npm install sequelize sequelize-cli
+- npm install bcryptjs
+- npm install jsonwebtoken
+- npm install dotenv
+- npm install cookie-parser
+
+### Cara Membuat Table & Database
+
+- npx sequelize-cli db:create
+- npm run start (Automatice migration table)
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+// Posts Router
+const postRouter = require('./routes/Posts');
+app.use('/posts', postRouter);
+
+// Comments Router
+const commentRouter = require('./routes/Comments');
+app.use('/comments', commentRouter);
+
+// Users Router
+const userRouter = require('./routes/Users');
+app.use('/auth', userRouter);
+
+// Likes Router
+const likeRouter = require('./routes/Likes');
+app.use('/likes', likeRouter);
+
+const db = require('./models');
+
+db.sequelize.
+    sync().
+    then(() => {
+        app.listen(process.env.PORT || 3001, () => {
+            console.log("Server running on port 3001");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
